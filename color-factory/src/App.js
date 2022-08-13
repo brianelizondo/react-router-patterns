314a4394dd0bd9a96143ee89020290a478ef79ea
Brian Elizondo <brian_elizondo@msn.com>
2022-08-13 15:52:16 -0400

    React Router Patterns => Part 2: React Router Color Factory - 2) As a user, I can click on one of the colors in my colors list and get taken to a page where I can see that color in all its glory. The route here should be /colors/:color

--- color-factory/src/App.js
+++ color-factory/src/App.js
@@ -1,5 +1,5 @@
-import {React, useState } from 'react';
-import { Switch, Route, Redirect } from "react-router-dom";
+import {React, useState, useEffect } from 'react';
+import { Switch, Route, Redirect, Link } from "react-router-dom";
 import './App.css';
 
 import ColorList from "./ColorList";
@@ -7,14 +7,27 @@ import ColorDetails from "./ColorDetails";
 import ColorNew from "./ColorNew";
 
 function App() {
-    const INITIAL_STATE = [];
+    const INITIAL_STATE = [
+        {
+            name: "black",
+            code: "#000000"
+        },
+        {
+            name: "white",
+            code: "#FFFFFF"
+        },
+        {
+            name: "green",
+            code: "#00FF00"
+        }
+    ];
     const [colors, setColors] = useState(INITIAL_STATE);
 
     return (
         <div className="App">
             <Switch>
                 <Route exact path="/colors/new"><ColorNew /></Route>
-                <Route exact path="/colors/:color"><ColorDetails /></Route>
+                <Route exact path="/colors/:color"><ColorDetails colors={colors} /></Route>
                 <Route exact path="/colors"><ColorList colors={colors} /></Route>
                 <Redirect to="/colors" />
             </Switch>
--- color-factory/src/ColorDetails.js
+++ color-factory/src/ColorDetails.js
@@ -1,10 +1,16 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
+import "./ColorDetails.css"
 
-function ColorDetails(){
+function ColorDetails({ colors }){
+    const { color } = useParams();
+    const colorData = colors.filter(colorArr => color === colorArr.name.toLowerCase())[0];
+
     return (
-        <div className="ColorDetails">
-            
+        <div className="ColorDetails" style={{ backgroundColor: colorData.code }}>
+            <div>this is <b>{ colorData.name }</b></div>
+            <div>isn't it beautiful?</div>
+            <div><Link to="/colors">go back</Link></div>
         </div>
     );
 }
